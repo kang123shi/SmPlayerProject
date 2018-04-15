c884aa197f4e14f2aed257cd5eef36258f54bafe
Administrator <450752177@qq.com>
2018-04-15 23:08:46 +0800

    添加http

---
+++ app/src/main/java/com/ksm/smplayerproject/Application/Http/API.kt
@@ -0,0 +1,8 @@
+package com.ksm.smplayerproject.Application.Http
+
+/**
+ * Created by Administrator on 2018/4/14.
+ */
+public class API  {
+    public val MAIN_API : String = "www.baidu.com"
+}
---
+++ app/src/main/java/com/ksm/smplayerproject/Application/Http/HttpApi.kt
@@ -0,0 +1,8 @@
+package com.ksm.smplayerproject.Application.Http
+
+/**
+ * Created by Administrator on 2018/4/14.
+ */
+public interface HttpApi {
+
+}
